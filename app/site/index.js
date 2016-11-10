6a2dc35a943b07a5735af4ebd9e38e7b65b5c028
Ronald M. Clifford <roncli@roncli.com>
2016-11-09 22:33:06 -0800

    - Include jQuery, Bootstrap, and Angular. - Implement basic functionality to add files to the grid and play them on demand.

--- app/site/index.js
+++ app/site/index.js
@@ -1,14 +1,67 @@
-onLoad = () => {
-    window.addEventListener("dragover", (ev) => {
-        ev.preventDefault();
-    }, false);
+var files = {},
+    filenameRegex = /[/\\]([^/\\]+)\.([^/\\]+)$/,
+    angular, app, scope;
 
-    window.addEventListener("drop", (ev) => {
+window.$ = window.jQuery = require("../node_modules/jquery/dist/jquery.min.js");
+
+angular = require("angular"),
+app = angular.module("chattergrid", []),
+require("../node_modules/bootstrap/dist/js/bootstrap.min.js");
+
+app.controller("chattergrid", ["$scope", function($scope) {
+    $scope.sounds = [];
+
+    $scope.play = (sound) => {
+        sound.audio.play();
+    };
+}]);
+
+$(document).ready(() => {
+    $(window).on("dragover", (ev) => {
+        ev.preventDefault();
+        return false;
+    });
+
+    $(window).on("drop", (ev) => {
         var files;
 
-        ev.preventDefault();
         if (ev.target.id === "drop") {
-            files = ev.dataTransfer.files;
+            [].concat.apply([], ev.originalEvent.dataTransfer.files).forEach((file) => {
+                checkFile(file.path);
+            });
         }
-    }, false);
+
+        ev.preventDefault();
+        return false;
+    });
+
+    scope = angular.element("html").scope();
+});
+
+checkFile = (file) => {
+    if (files[file]) {
+        alert("You've already added " + file + ".");
+        return;
+    }
+
+    var audio = new Audio(file);
+
+    audio.onerror = (err) => {
+        alert(file + " is not of a supported format.");
+        err.cancelBubble = true;
+        audio = null;
+    };
+
+    audio.onloadeddata = () => {
+        var parsed = filenameRegex.exec(file);
+
+        files[file] = true;
+        scope.sounds.push({
+            file: file,
+            name: parsed[1],
+            type: parsed[2],
+            audio: audio
+        });
+        scope.$apply();
+    };
 };
